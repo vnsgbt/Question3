4fc4b05aa8ff37ce2d02a3e7cbf02181cab99915
unknown <snguy@PK1KT79.na.ds.monsanto.com>
2016-01-25 12:44:15 -0600

    Use args

--- src/main/java/org/vnsgbt/infoplus/App.java
+++ src/main/java/org/vnsgbt/infoplus/App.java
@@ -4,6 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CommaFormatNumber formatter = new CommaFormatNumber(args[0]);
+        System.out.println(formatter.formatCurrentNumber());
     }
 }
+
